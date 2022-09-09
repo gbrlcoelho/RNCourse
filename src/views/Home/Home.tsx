e876d4579ca5e6ea122649ae9c388f4aefa82ea5
Gabriel Coelho <coelhogabriel67@gmail.com>
2022-09-08 22:36:04 -0300

    refactor(views): refactored home view flatlist

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,17 +1,37 @@
 import React, {useState} from 'react'
-import {Button, ButtonText, Container, HomeTitle, Input, TasksTitle} from './HomeBase'
+import {FlatList} from 'react-native'
+import {Button, ButtonText, Container, HomeTitle, Input, TaskButton, TasksListText, TasksTitle} from './HomeBase'
+import {TaskList} from './HomeTypes'
 
 export const Home = () => {
   const [newTask, setNewTask] = useState('')
+  const [taskList, setTaskList] = useState<TaskList[]>([])
+
+  const handleAddNewTask = () => {
+    const data = {
+      id: String(new Date().getTime()),
+      title: newTask ? newTask : 'Empty task!',
+    }
+    setTaskList([...taskList, data])
+  }
 
   return (
     <Container>
       <HomeTitle>Hello Dev!</HomeTitle>
       <Input placeholder='Nova Tarefa' placeholderTextColor='#555' onChangeText={setNewTask} />
       <Button activeOpacity={0.7}>
-        <ButtonText>Adicionar</ButtonText>
+        <ButtonText onPress={handleAddNewTask}>Adicionar</ButtonText>
       </Button>
       <TasksTitle>Minhas Tarefas</TasksTitle>
+      <FlatList
+        data={taskList}
+        keyExtractor={(item) => item.id}
+        renderItem={({item}) => (
+          <TaskButton>
+            <TasksListText>{item.title}</TasksListText>
+          </TaskButton>
+        )}
+      />
     </Container>
   )
 }
