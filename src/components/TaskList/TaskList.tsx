ed67bdd1631945cbc2d8aba64ace6e2f038cca56
Gabriel Coelho <coelhogabriel67@gmail.com>
2022-09-10 10:42:04 -0300

    refactor(views/components): refactored Home view and Task List component

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,15 +1,29 @@
 import React from 'react'
-import {FlatList} from 'react-native'
+import {Alert, FlatList} from 'react-native'
+import {useTasks} from '../../hooks/useTasks'
 import {TaskButton, TasksListText} from '../TaskListBase'
-import {TaskListProps} from './TaskListTypes'
 
-export const TaskList = ({data}: TaskListProps) => {
+export const TaskList = () => {
+  const {tasksData, removeTask} = useTasks()
+
+  const handleRemoveTask = (id: string) => {
+    Alert.alert('Tem certeza?', 'Deseja excluir a tarefa?', [
+      {text: 'Cancelar', onPress: () => {}},
+      {
+        text: 'Excluir',
+        onPress: () => {
+          removeTask(id)
+        },
+      },
+    ])
+  }
+
   return (
     <FlatList
-      data={data}
+      data={tasksData}
       keyExtractor={(item) => item.id}
       renderItem={({item}) => (
-        <TaskButton>
+        <TaskButton onPress={() => handleRemoveTask(item.id)}>
           <TasksListText>{item.title}</TasksListText>
         </TaskButton>
       )}
