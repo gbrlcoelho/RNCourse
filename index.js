bdb08d1b0ea744c242a0a40a59831ecbf699ee39
Gabriel Coelho <coelhogabriel67@gmail.com>
2022-09-10 19:38:52 -0300

    refactor(global): disable font scalling

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import {AppRegistry, Text, TextInput} from 'react-native'
+import App from './App'
+import {name as appName} from './app.json'
 
-AppRegistry.registerComponent(appName, () => App);
+TextInput.defaultProps = TextInput.defaultProps || {}
+TextInput.defaultProps.allowFontScaling = false
+Text.defaultProps = Text.defaultProps || {}
+Text.defaultProps.allowFontScaling = false
+AppRegistry.registerComponent(appName, () => App)
